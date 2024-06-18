db45e8c6c8a91c90131b3d4ec2071bc41d4f7566
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2024-06-18 15:19:27 -0700

    added global css file for tailwind config, updated layout ts file, page ts file

---
+++ test-ui/src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -0,0 +1,26 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+import type { Metadata } from 'next'
+
+import config from '@payload-config'
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+
+type Args = {
+  params: {
+    segments: string[]
+  }
+  searchParams: {
+    [key: string]: string | string[]
+  }
+}
+
+export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
+  generatePageMetadata({ config, params, searchParams })
+
+const Page = ({ params, searchParams }: Args) => RootPage({ config, params, searchParams })
+  return (
+<div>
+  <h1 className="text-6x1 font-bold text-red-500">My home page</h1>
+</div>
+  )
+export default Page
---
+++ test-ui/src/app/(payload)/layout.tsx
@@ -0,0 +1,22 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+import configPromise from '@payload-config'
+import '@payloadcms/next/css'
+import { RootLayout } from '@payloadcms/next/layouts'
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import React from 'react'
+
+import './custom.scss'
+import './globals.css'
+
+type Args = {
+  children: React.ReactNode
+}
+
+const Layout = React.FC<LayoutProps> = ({ children }( => {
+  return (
+    <html>
+    <body>{children}</body> 
+    </html>
+  )
+ }
+export default Layout
